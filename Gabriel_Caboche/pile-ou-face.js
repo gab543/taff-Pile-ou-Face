0aaf28baeb16f60e84c69a7767ad91f108ef74eb
gab543 <58109378+gab543@users.noreply.github.com>
2025-11-14 19:09:45 +0100

    Update pile-ou-face.js

--- Gabriel_Caboche/pile-ou-face.js
+++ Gabriel_Caboche/pile-ou-face.js
@@ -1,18 +1,32 @@
 
 let options = ["true", "false"]
-let choixUser = prompt("Pile ou Face ?").toLowerCase()
-
-while(options.includes(choixUser)){
-}
+let etat_piece = "";
+let replay = true;
 
 function lancer(){
-    etat_piece = Math.random(1, 2);
+    etat_piece = Math.floor(Math.random() * 2) + 1;
     if (etat_piece === 1) {
-        etat_piece = "pile"
+        etat_piece = "pile";
     }
-    elseif (etat_piece === 2) {
-        etat_piece = "face"
+    else if (etat_piece === 2) {
+        etat_piece = "face";
     }
-    console.log(etat_piece)
-    return etat_piece
+    console.log(etat_piece);
+    return etat_piece;
+}
+while (replay == true) {
+    let choixUser = prompt("Pile ou Face ?").toLowerCase();
+    while (choixUser !== "pile" && choixUser !== "face") {
+        choixUser = prompt("Veuillez choisir entre Pile ou Face").toLowerCase()
+    }
+
+    lancer();
+
+    if (choixUser === etat_piece) {
+        alert("Gagné !");
+        replay = confirm("Voulez-vous rejouer ?");
+    } else {
+        alert("Perdu !");
+        replay = confirm("Voulez-vous rejouer ?");
+    }
 }
